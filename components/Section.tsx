d5d2dcfe0d5cee4a58f82cd7d9e9bc252c413da0
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-12-09 22:38:45 +0530

    update about page

---
+++ components/Section.tsx
@@ -0,0 +1,29 @@
+"use client"
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+type SectionProps = {
+  heading: string;
+  headingAlignment?: "right" | "left";
+  children: ReactNode;
+};
+
+export default function Section({
+  heading,
+  headingAlignment,
+  children,
+}: SectionProps) {
+  return (
+    <section className="flex flex-col md:flex-row gap-2 md:gap-9 col-reverse">
+      <h2
+        className={clsx(
+          "md:w-32 text-secondary shrink-0 ",
+          headingAlignment === "right" && "md:text-right"
+        )}
+      >
+        {heading}
+      </h2>
+      {children}
+    </section>
+  );
+}
