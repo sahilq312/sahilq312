3dd45db4161c3700b136c6b751a5a5511d2335e4
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-12-12 01:45:56 +0530

    commit

--- lib/ProjectData.ts
+++ lib/ProjectData.ts
@@ -1,27 +1,32 @@
+import Avatar from "@/public/avatar.png"
+
 const projects = [
     {
         id: 1,
         title: "Ecommerce-app",
-        image: "",
+        image: "https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Famazon-clone.jpeg&w=750&q=75",
         description: "A MERN stack ecommerce app . ",
         tech: ["TypeScript" , "Redux" , "Vite" , "MongoDB" ],
         link: "https://github.com/sahilq312/e-commerce",
+        date: 2022
     },
     {
         id: 2,
         title: "Next.js Ecommece-app ",
-        image: "",
+        image: "https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Famazon-clone.jpeg&w=750&q=75",
         description: "A MERN stack ecommerce app .  ",
-        tech: [ "TypeScript", "Next.js" , "Tailwind" , ""],
+        tech: [ "TypeScript", "Next.js" , "Tailwind" ,],
         link: "https://github.com/sahilq312/shopz",
+        date : 2022
     },
     {
         id: 3,
         title: "Twitter-clone",
-        image: "",
+        image: "https://imgs.search.brave.com/SGeBPET_Dlj65jbA5zTwiFWbRy0aVjXRIzSfBJ374WM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXNp/bmVzcy50d2l0dGVy/LmNvbS9jb250ZW50/L2RhbS9idXNpbmVz/cy10d2l0dGVyL2Vu/L3Jlc291cmNlcy9i/bG9nL3EzLTIwMjIv/VHdpdHRlci1pbWFn/ZS1hZC5wbmcudHdp/bWcuMTkyMC5wbmc",
         description: "A MERN stack Social Media app . ",
         tech: ["JavaScript", "Cloudinary", "Vite", "Tailwind"],
         link: "https://github.com/sahilq312/fb",
+        date: 2022
     },
 
 ]
